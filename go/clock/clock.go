8e63ca4f7ba49a1db77bbd0cb802145072011263
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2016-03-18 22:22:42 -0300

    Create the docs for packages and functions

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,3 +1,4 @@
+// Package clock implements a simple library to handle time without dates
 package clock
 
 import (
@@ -6,10 +7,12 @@ import (
 
 const testVersion = 3
 
+// A Clock represets the clock with hour and minutes
 type Clock struct {
 	h, m int
 }
 
+// Create a new clock representing a clock with hours and minutes
 func New(hour, minute int) Clock {
 	hour = (hour + minute/60) % 24
 	minute = minute % 60
@@ -23,10 +26,13 @@ func New(hour, minute int) Clock {
 	return Clock{hour, minute}
 }
 
+// Return a string representing the actual state of the clock. pattern: hh:mm
 func (c Clock) String() string {
 	return fmt.Sprintf("%02d:%02d", c.h, c.m)
 }
 
+// Add an quantity of minutes on the clock.
+// Negative numbers will back the clock
 func (c Clock) Add(minutes int) Clock {
 	var hour = (c.h + (c.m+minutes)/60) % 24
 	minutes = (minutes + c.m) % 60
--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,7 +1,16 @@
+//Package leap implements one function to
+//discover if a year is leap or not
 package leap
 
 const testVersion = 2
 
+/*
+IsLeapYear take a year and returns if that year is a leap year
+with these rules:
+	on every year that is evenly divisible by 4
+	except every year that is evenly divisible by 100
+	unless the year is also evenly divisible by 400
+*/
 func IsLeapYear(year int) bool {
 	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
 }
