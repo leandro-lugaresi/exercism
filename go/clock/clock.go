8f328b755b63052491354348adfa99573ee991b6
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2016-03-18 23:28:38 -0300

    Remove unnecessary code on Add funcion

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -6,6 +6,10 @@ import (
 )
 
 const testVersion = 3
+const (
+	dayInHours    = 24
+	hourInMinutes = 60
+)
 
 // A Clock represets the clock with hour and minutes
 type Clock struct {
@@ -14,14 +18,14 @@ type Clock struct {
 
 // Create a new clock representing a clock with hours and minutes
 func New(hour, minute int) Clock {
-	hour = (hour + minute/60) % 24
-	minute = minute % 60
+	hour = (hour + minute/hourInMinutes) % dayInHours
+	minute = minute % hourInMinutes
 	if minute < 0 {
 		hour--
-		minute = minute + 60
+		minute = minute + hourInMinutes
 	}
 	if hour < 0 {
-		hour = hour + 24
+		hour = hour + dayInHours
 	}
 	return Clock{hour, minute}
 }
@@ -34,14 +38,5 @@ func (c Clock) String() string {
 // Add an quantity of minutes on the clock.
 // Negative numbers will back the clock
 func (c Clock) Add(minutes int) Clock {
-	var hour = (c.h + (c.m+minutes)/60) % 24
-	minutes = (minutes + c.m) % 60
-	if minutes < 0 {
-		hour--
-		minutes = minutes + 60
-	}
-	if hour < 0 {
-		hour = hour + 24
-	}
-	return Clock{hour, minutes}
+	return New(c.h, c.m+minutes)
 }
