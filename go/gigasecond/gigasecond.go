6d54676610fc82c992beda14bcddd5db026ecc70
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2016-03-27 18:54:31 -0300

    Implement the AddGigasecond function

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -1,10 +1,14 @@
-// Package clause.
+// Package gigasecond implements a simple library to calculate the 1 Gs anniversary.
+// A gigasecond is one billion (10**9) seconds.
 package gigasecond
 
-// Constant declaration.
-const testVersion = ? // find the value in gigasecond_test.go
+import "time"
 
-// API function.  It uses a type from the Go standard library.
-func AddGigasecond(time.Time) time.Time
+// Test version.
+const testVersion = 4
 
-// Reviewers don't think much of stub comments.  Replace or remove.
+// AddGigasecond returns the time.Time + 1e9 seconds.
+func AddGigasecond(date time.Time) time.Time {
+	var d = time.Duration(1e9 * time.Second)
+	return date.Add(d)
+}
