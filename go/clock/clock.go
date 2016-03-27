86936d677c5a66b33575b90292a952d22eba3d1c
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2016-03-27 17:30:28 -0300

    combine the hours and minutes to simplify the adjustments

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,4 +1,4 @@
-// Package clock implements a simple library to handle time without dates
+// Package clock implements a simple library to handle time without dates.
 package clock
 
 import (
@@ -7,38 +7,34 @@ import (
 
 const testVersion = 3
 
-// Constants to represent the fixed number of hours in a day and minutes in a hour
+// Constants to represent the fixed number of hours in a day and minutes in a hour.
 const (
-	dayInHours    = 24
-	hourInMinutes = 60
+	dayInHours      = 24
+	hourInMinutes   = 60
+	minutesInOneDay = 1440
 )
 
-// A Clock represets the clock with hour and minutes
+// A Clock represets the clock using the minutes from 0 to 1440 minutos.
 type Clock struct {
-	h, m int
+	time int
 }
 
-// Create a new clock representing a clock with hours and minutes
-func New(hour, minute int) Clock {
-	hour = (hour + minute/hourInMinutes) % dayInHours
-	minute = minute % hourInMinutes
-	if minute < 0 {
-		hour--
-		minute = minute + hourInMinutes
+// Create a new clock representing a clock with hours and minutes.
+func New(hours, minutes int) Clock {
+	minutes = (minutes + hours*hourInMinutes) % minutesInOneDay
+	if minutes < 0 {
+		minutes = minutes + minutesInOneDay
 	}
-	if hour < 0 {
-		hour = hour + dayInHours
-	}
-	return Clock{hour, minute}
+	return Clock{minutes}
 }
 
-// Return a string representing the actual state of the clock. pattern: hh:mm
+// Return a string representing the actual state of the clock. pattern: hh:mm.
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.h, c.m)
+	return fmt.Sprintf("%02d:%02d", c.time/hourInMinutes, c.time%hourInMinutes)
 }
 
 // Add an quantity of minutes on the clock.
-// Negative numbers will back the clock
+// Negative numbers will back the clock.
 func (c Clock) Add(minutes int) Clock {
-	return New(c.h, c.m+minutes)
+	return New(0, c.time+minutes)
 }
